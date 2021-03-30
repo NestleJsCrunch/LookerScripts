922c3ce5a2a6d1e127918aef65640643819be87d
James Nestler <44789505+NestleJsCrunch@users.noreply.github.com>
2021-03-30 15:42:36 -0700

    Update TabbedDownload_Look.py
    added better tab titles

--- TabbedDownload_Look.py
+++ TabbedDownload_Look.py
@@ -2,12 +2,11 @@
 !pip install looker-sdk
 import looker_sdk
 
-# I'm using colab and setting these as environment variables. you would normally place these in the sdk config file
 import os
-os.environ['LOOKERSDK_BASE_URL'] = '<insert api url>'
+os.environ['LOOKERSDK_BASE_URL'] = 'https://dcl.api.dev.looker.com'
 os.environ['LOOKERSDK_API_VERSION'] = '3.1'
-os.environ['LOOKERSDK_CLIENT_ID'] = '<client id>'
-os.environ['LOOKERSDK_CLIENT_SECRET'] = '<client secret>'
+os.environ['LOOKERSDK_CLIENT_ID'] = 'BPdgzrT9nqrJHjwbJByn'
+os.environ['LOOKERSDK_CLIENT_SECRET'] = 'Kyg2D2xdd95B4RpYc3RRBgPN'
 
 # init the sdk
 sdk = looker_sdk.init31("looker.ini")
@@ -15,7 +14,7 @@ sdk = looker_sdk.init31("looker.ini")
 import pandas as pd
 import csv
 
-# colab specific 
+# doing this in colab
 from google.colab import files
 
 !pip install -U -q PyDrive
@@ -30,18 +29,27 @@ gauth = GoogleAuth()
 gauth.credentials = GoogleCredentials.get_application_default()
 drive = GoogleDrive(gauth)
 
-### the actual loop ###
+### the actual loop
 
-looks = [<comma delimited list of Look 'IDs', eg: '1','2','3'>]
+# initialize variables
+looks = ['2131', '2132', '2133']
 lislen = len(looks)
+titles = []
 
+# loop to get look titles for tabs
+for i in range(lislen):
+  data = sdk.look(looks[i])
+  title = data.title
+  titles.append(title)
+
+# loop to create the tabbed output file
 with pd.ExcelWriter('output.xlsx') as writer: 
   for i in range(lislen):
     data = sdk.run_look(looks[i],'csv')
     with open('big.csv', 'w', newline='') as file:
         file.write(data)
     df = pd.read_table('big.csv', delimiter =",",engine='python')
-    df.to_excel(writer, sheet_name=('Tab '+ str(i)))
+    df.to_excel(writer, sheet_name=(titles[i]))
   
 # colab specific
   files.download('output.xlsx')
